07d6a3b737879444569d56847a4c058ee015443e
imyidkwhy <imyidkwhy@gmail.com>
2025-07-04 09:33:10 +0300

    styles added

--- src/script.js
+++ src/script.js
@@ -1,3 +1,28 @@
-async function getSong() {
-    try
+const coversGrid = document.getElementById('covers-grid')
+
+
+async function getSongs() {
+    try {
+        const response = await fetch('./songs.json');
+        const songs = await response.json()
+        console.log(songs);
+        function displaySongsCover(songs) {
+            songs.forEach(song => {
+                const songCard = document.createElement('div')
+                songCard.classList.add('song-card')
+                songCard.innerHTML = `
+                <img src="${song.cover}" alt="${song.title}">
+                <p>${song.title}</p>
+                 `
+                coversGrid.appendChild(songCard);
+            })
+
+
+        }
+        displaySongsCover(songs)
+    } catch (error) {
+        console.error("an error occured", error);
+
+    }
 }
+getSongs()
